9faf12f5dc8d804d865fdc4fc76c25ced73b9bf3
Zexin Young <lllidanismine@gmail.com>
2017-10-18 21:41:36 +0800

    完成子弹的创建

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,10 @@ import pygame
 
 from settings import Settings
 from  ship import  Ship
+from pygame.sprite import Group
+
 import game_functions as gf
+
 def run_game():
     # 初始化游戏并创建一个屏幕对象
     pygame.init()
@@ -19,6 +22,10 @@ def run_game():
     # 创建一艘飞船
     ship = Ship(ai_settings, screen)
 
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
+
+
     # 设置游戏主循环
 
     bg_color = (230,230,230)
@@ -26,9 +33,14 @@ def run_game():
     while True:
 
         """响应按键和鼠标事件"""
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
+        bullets.update()
+
+        """更新子弹的位置，并删除已消失的子弹"""
+        gf.update_bullets(bullets)
+
         """更新屏幕上的图像， 并切换到新屏幕"""
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 run_game()
--- settings.py
+++ settings.py
@@ -10,3 +10,10 @@ class Settings():
 
         # 飞船的设置
         self.ship_speed_factor = 1.5
+
+        # 子弹设置
+        self.bullet_speed_factor = 2
+        self.bullet_width = 5
+        self.bullet_height = 20
+        self.bullet_color = 60, 60, 60
+        self.bullets_allowed = 3
