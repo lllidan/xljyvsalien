43328c274787c1e558da9af6ec2d134012da2927
Zexin Young <lllidanismine@gmail.com>
2017-10-23 23:06:23 +0800

    完成14.2 提高游戏难度

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from  ship import  Ship
 from pygame.sprite import Group
 from  alien import Alien
 from game_stats import GameStats
+from Button import Button
 import game_functions as gf
 
 def run_game():
@@ -34,11 +35,16 @@ def run_game():
     # 设置游戏主循环
 
     bg_color = (230,230,230)
+
+    # 创建play按钮
+    play_button = Button(ai_settings, screen, "Play")
+
     # 开始游戏的主循环
     while True:
 
         """响应按键和鼠标事件"""
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, play_button, ship, aliens,
+                  bullets)
         if stats.game_active:
             ship.update()
 
@@ -49,6 +55,7 @@ def run_game():
             gf.update_aliens(ai_settings,stats, screen, ship, aliens, bullets)
 
         """更新屏幕上的图像， 并切换到新屏幕"""
-        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets,
+                         play_button)
 
 run_game()
